e409a6c8f226a2f872705e76afd2a0b09b83fe99
jonaskn <jonaskn@users.noreply.github.com>
2016-01-27 09:30:52 -0600

    added licence information before switching to public

--- edfmex.m
+++ edfmex.m
@@ -66,14 +66,19 @@
 %
 %        mex edfmex.cpp edfapi.lib
 % 
-% Written by C. Kovach 2007 and released for non-commercial use 
-% without any gaurantee whatsoever. I am not responsible for any 
-% loss or corruption of data, damage to hardware, personal injury, 
-% marital discord or any other disaster arising from the use of 
-% this software. 
-%
-% Questions can be sent to christopher-kovach@uiowa.edu.
+%   Copyright (C) 2009  Christopher K. Kovach, 2015 Christopher K. Kovach & Jonas Knöll
 % 
-% Last modified May 23rd 2015 by Jonas Knöll (jonas.knoell@utexas.edu) 
-% to reduce memory overhead and allow reading of a selection of sample 
-% fields only.
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU Affero General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+% 
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU Affero General Public License for more details.
+% 
+%   You should have received a copy of the GNU Affero General Public License
+%   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+%
+% Questions can be sent to christopher-kovach@uiowa.edu or Jonas Knöll (jonas.knoell@utexas.edu) 
